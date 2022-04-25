da1f4628ec5887d1b0a45700afca54eccc36b93c
Hanest <53016199+Hanestx@users.noreply.github.com>
2022-04-25 12:33:28 +0400

    HW_02_Asteroids_Metoda

---
+++ Architecture&Patterns/HW_02_Asteroids/04_GB_Patterns_Asteroids/Assets/Code/MoveTransform.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Asteroids
+{
+    internal class MoveTransform : IMove
+    {
+        private readonly Transform _transform;
+        public float Speed { get; protected set; }
+        private Vector3 _move;
+
+        public MoveTransform(Transform transform, float speed)
+        {
+            _transform = transform;
+            Speed = speed;
+        }
+
+        public void Move(float horizontal, float vertical, float deltaTime)
+        {
+            var speed = deltaTime * Speed;
+            _move.Set(horizontal * speed, vertical * Speed, 0.0f);
+            _transform.localPosition += _move;
+        }
+    }
+}
